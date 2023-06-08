56cad2cb9a6c926d74c61262f78c06a97ee95681
Alex <reybislp@gmail.com>
2023-06-08 01:21:59 -0400

    add footer

---
+++ src/components/Footer/Cookies.tsx
@@ -0,0 +1,12 @@
+import { quickSands } from "@/utils/fonts"
+
+const Cookies = () => {
+  return (
+    <div className={`flex justify-center text-center content-center items-center gap-2 ${quickSands.className} p-4`}>
+      <p>This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you whisk.</p>
+      <span className="bg-black text-white p-2">Accept</span>
+    </div>
+  )
+}
+
+export default Cookies
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,80 @@
+import Image from "next/image";
+import footerLogo from "@/assets/logo/logo-w.png";
+import fb from "@/assets/logo/facebook.svg";
+import { francois, quickSands } from "@/utils/fonts";
+import Link from "next/link";
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import Cookies from "./Cookies";
+const Footer = () => {
+  return (
+    <>
+      {/* <div className="waves"></div> */}
+      <footer className="bg-[#FBB660] waves">
+        <div className="container mx-auto px-4 py-8">
+          <div className="flex items-center justify-center mb-4">
+            <Image className="" src={footerLogo} alt="Logo" priority />
+          </div>
+          <div className="flex justify-center uppercase ">
+            <Link
+              href="/"
+              className={`mr-8 transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              shop
+            </Link>
+            <Link
+              href="/"
+              className={`mr-8 transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              sale
+            </Link>
+            <Link
+              href="/"
+              className={`mr-8 transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              girls
+            </Link>
+            <Link
+              href="/"
+              className={`mr-8 transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              boys
+            </Link>
+            <Link
+              href="/"
+              className={`mr-8 transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              lookbook
+            </Link>
+            <Link
+              href="/"
+              className={`transition-all text-[18px] ${francois.className} text-white hover:text-gray-300`}
+            >
+              contacts
+            </Link>
+          </div>
+          <div className="flex justify-center uppercase pt-7 ">
+            <FaFacebookF className="transition-all mr-4 text-white w-5 h-5 cursor-pointer hover:text-gray-300" />
+            <FaInstagram className="transition-all mr-4 text-white w-5 h-5 cursor-pointer hover:text-gray-300" />
+            <FaTwitter className="transition-all text-white w-5 h-5 cursor-pointer hover:text-gray-300" />
+          </div>
+          <span
+            className={`flex justify-center mt-5 text-white ${quickSands.className} `}
+          >
+            <Link
+              className={`mr-2 ${francois.className} transition-all hover:text-gray-300`}
+              href="/"
+            >
+              {" "}
+              Privacy Policy{" "}
+            </Link>{" "}
+            / This is a sample website - cmsmasters ©{" "}
+            {` ${new Date().getFullYear()} `} / All Rights Reserved
+          </span>
+        </div>
+      </footer>
+      <Cookies />
+    </>
+  );
+};
+
+export default Footer;
