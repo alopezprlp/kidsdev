e1d9dc57e637d8842a538c6fdb5e8e38073b8a71
Alex <reybislp@gmail.com>
2023-06-08 01:21:38 -0400

    add utils functions

---
+++ src/utils/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+const quickSands = localFont({ src: "../assets/fonts/Quicksand-Regular.ttf" });
+const francois = localFont({ src: "../assets/fonts/FrancoisOne-Regular.ttf" });
+const gilda = localFont({ src: "../assets/fonts/GildaDisplay-Regular.ttf" });
+
+export { quickSands, francois, gilda };
