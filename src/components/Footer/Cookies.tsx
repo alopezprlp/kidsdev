7769a83b70c73da6f3d57620f4585d878467c24f
Alex <reybislp@gmail.com>
2023-06-08 11:44:35 -0400

    add footer bottom

--- src/components/Footer/Cookies.tsx
+++ src/components/Footer/Cookies.tsx
@@ -4,7 +4,7 @@ const Cookies = () => {
   return (
     <div className={`flex justify-center text-center content-center items-center gap-2 ${quickSands.className} p-4`}>
       <p>This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you whisk.</p>
-      <span className="bg-black text-white p-2">Accept</span>
+      <span className="bg-black text-white p-2 cursor-pointer transition-all hover:shadow-lg">Accept</span>
     </div>
   )
 }
