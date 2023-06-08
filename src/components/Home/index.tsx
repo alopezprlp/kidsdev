5598c801c55761a30498c1a259b0d3efa2524e5c
Alex <reybislp@gmail.com>
2023-06-08 12:54:53 -0400

    add c banner

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,9 +1,11 @@
+import CatBanner from "./CategoryBanner";
 import Hero from "./Hero";
 
 const HomePage = () => {
   return (
     <main className="container mx-auto px-28 py-8 mb-40 ">
       <Hero />
+      <CatBanner />
     </main>
   );
 };
