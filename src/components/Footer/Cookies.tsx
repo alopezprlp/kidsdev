3bd53d574ee095325919a74ce379e8b891ee743d
Alex <reybislp@gmail.com>
2023-06-11 14:17:30 -0400

    fix footer link

--- src/components/Footer/Cookies.tsx
+++ src/components/Footer/Cookies.tsx
@@ -9,11 +9,11 @@ const Cookies = () => {
   return (
     <div
     className={clsxm(
-      `absolute w-full transition-all duration-500 flex justify-center text-center content-center items-center gap-2 ${quickSands.className} p-4`,
+      `absolute -z-50 w-full transition-all duration-500 flex justify-center text-center content-center items-center gap-2 ${quickSands.className} p-4`,
       !hidde ? "h-16" : "h-0 p-0",
       !hidde ? "opacity-100" : "opacity-0",
       !hidde ? "translate-y-[0px]" : "translate-y-[-50px]",
-      // !hidde ? "flex" : "hidden",
+      !hidde ? "flex" : "hidden",
     )}>
       <p>This website uses cookies to improve your experience. We'll assume you're ok with this, but you can opt-out if you whisk.</p>
       <span onClick={()=>setHidde(true)} className="bg-black text-white p-2 cursor-pointer transition-all hover:shadow-lg">Accept</span>
